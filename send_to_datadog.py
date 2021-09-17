001720b6d2f2fdbfa91745dec410953c1a4f2576
Adam Berman <adam@returntocorp.com>
2021-09-17 01:20:07 -0700

    add status

--- send_to_datadog.py
+++ send_to_datadog.py
@@ -12,13 +12,14 @@ jobs_resp = requests.get(jobs_url)
 jobs = jobs_resp.json()['jobs']
 scan_job_status = [j for j in jobs if j["name"] == "Scan"][0]["conclusion"]
 
-text = "success" if scan_job_status == "success" else "failure"
+text = "success" if scan_job_status == "success" else "error"
 
 response = requests.post("https://api.datadoghq.com/api/v1/events",
               headers = { "DD-API-KEY": os.environ.get("DD_API_KEY") },
               json={
                   "title": "github-actions-scan",
                   "text": text,
+                  "status": text,
               })
 
 print(response.status_code)
